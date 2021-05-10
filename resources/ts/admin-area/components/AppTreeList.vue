3fbef45e728fc83d16b99af5724c7512e51f1491
Petr Zenin <mr@adminoid.com>
2021-05-10 16:47:02 +0300

    sortable tree skeleton

--- resources/ts/admin-area/components/AppTreeList.vue
+++ resources/ts/admin-area/components/AppTreeList.vue
@@ -1,18 +1,40 @@
 <template lang="pug">
 ul
-    li(v-for="item in list") {{ item.name }}
-        app-tree-list(
-            v-if="item.hasOwnProperty('children')"
-            :list="item.children"
-        )
+    li(v-for="(item, i) in list" :ref="el => { if (el) elements[i] = el }") {{ item.name }}
+        app-tree-list(:list="item.children")
 </template>
 
 <script lang="ts">
-import {defineComponent} from 'vue'
+import {defineComponent, onMounted, reactive} from 'vue'
+import Sortable from 'sortablejs'
 
 const AppTreeList = defineComponent({
     props: {
         list: Array,
+    },
+
+    setup(props) {
+        const elements = reactive([])
+        onMounted(() => {
+            const length = props.list?.length
+            if (length) {
+                for (let i = 0; i < length; i++) {
+                    if (elements) {
+                        new Sortable(elements[i], {
+                            group: 'nested',
+                            animation: 150,
+                            fallbackOnBody: true,
+                            swapThreshold: 0.65
+                        })
+                    }
+                }
+            }
+        })
+
+        return {
+            elements,
+        }
+
     },
 })
 
